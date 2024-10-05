b4528cc597a1240b83867e8ed160a9b46639b7b4
spiderdog22 <spiderdog22@gmail.com>
2024-10-05 18:21:36 -0500

    Implemeta el envio de correos

--- js/send_email.js
+++ js/send_email.js
@@ -2,3 +2,25 @@ emailjs.init({
     publicKey: 'T29U9qPCcfa6SMJXA'
 });
 
+const form = document.querySelector('form');
+
+const sendEmail = (evt) => {
+    evt.preventDefault();
+
+    const parameters = {
+        from_name: form.fname.value,
+        email_dest: form.email.value,
+        message: form.message.value
+    };
+
+    emailjs.send('app_correos', 'friendly_message', parameters).then(
+        (response) => { alert('Mail has been sent!'); },
+        (error) => { alert('Couldn\' send the mail.'); }
+    );
+
+    form.fname.value = '';
+    form.email.value = '';
+    form.message.value = '';
+};
+
+form.addEventListener('submit', sendEmail);
